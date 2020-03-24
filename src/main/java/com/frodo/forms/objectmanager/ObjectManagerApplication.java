f0615329d3167faecf6d6d9ae45938d71ef11ced
Alexander Gonzalez <lex1985@bitbucket.org>
2020-03-24 18:05:22 -0300

    First Commit

---
+++ src/main/java/com/frodo/forms/objectmanager/ObjectManagerApplication.java
@@ -0,0 +1,13 @@
+package com.frodo.forms.objectmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjectManagerApplication.class, args);
+	}
+
+}
